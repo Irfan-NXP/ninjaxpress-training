830ab0d406cd6f9d089a62870ed2cf0c86bdd1ef
unknown <irfan.juanda@ninjavan.co>
2021-10-25 14:57:35 +0700

    Simpan semua file Pert-1

---
+++ Pert-1.py
@@ -0,0 +1,30 @@
+# print = keyword Python
+print('Hello, World!')
+
+# Variable
+x1 = 5
+x2 = 2
+y = 'Hello, Ninja!'
+
+print(x1)
+print(x1 * x2)
+print(y) #string
+
+# Python execute in sequence dari atas
+
+x = 5 #integer
+z = 0.1 #float
+w = True #boolean
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(w))
+
+print(x1 / x2)
+print(x1 % x2)
+
+n = 6%2
+print(type(n))
+print(n)
+
+print(round(0.4))
