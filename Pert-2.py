3f52a97250c4cc5fe422ea61e26dadd05b282ce5
unknown <irfan.juanda@ninjavan.co>
2021-10-25 16:21:51 +0700

    pert 2 data

---
+++ Pert-2.py
@@ -0,0 +1,36 @@
+# name = input("Enter Name: ")
+# print("Your Name is "+ name)
+
+# Operasi terhadap tipe data (String)
+#a = "Hello World!"
+#b = "Have a Nice Day!"
+#c = a + " " + b
+
+#print(len(a))
+#print(a[0:5])
+
+#a = a.replace('World', 'Ninja')
+
+#nama = 'Irfan'
+#alamat = 'Jakarta'
+
+#profile = 'Name saya {} dan saya tinggal di {}'.format(nama, alamat)
+
+#print(profile)
+
+#BOOLEAN
+# a = 10
+# b = 9
+
+# if b > a:
+#     print('b is greater than a')
+# else:
+#     print('a is greater than b')
+
+#LIST
+my_list = []
+my_list.append(1)
+my_list.append("My String")
+my_list.append(10 > 9)
+
+print(my_list)
