23a13c9101438d8aa48d900caaadcad1b47777db
unknown <irfan.juanda@ninjavan.co>
2021-10-29 16:44:57 +0700

    pert-4

---
+++ Pert-4.py
@@ -0,0 +1,85 @@
+# DICTIONARY
+# pelanggan = {
+#     "nama" : "Irfan",
+#     "umur" : 19
+# }
+
+# pelanggan2 = {
+#     "nama" : "Juanda",
+#     "umur" : 20
+# }
+
+# for x in pelanggan:
+#     print(x)
+#     print(pelanggan[x])
+#     print(pelanggan2[x])
+
+# daftar_pelanggan = []
+# daftar_pelanggan.append(pelanggan)
+# daftar_pelanggan.append(pelanggan2)
+
+# for pelanggan in daftar_pelanggan:
+#     print("Nama = {}".format(pelanggan["nama"]))
+#     print("Umur = {}".format(pelanggan["umur"]))
+
+# mydict = {
+#     'name':'Zara',
+#     'age':7, 
+#     'Class':'First'}
+
+# mydict['age'] = 8;
+# mydict['school'] = "DPS School"
+
+# print("dict['age']: ", mydict['age'])
+# print("dict['school']: ", mydict['school'])
+
+# FUNCTION
+# def myfunction(nama):
+#     print("Hello {}".format(nama))
+
+# myfunction("Irfan")
+
+# def myfunction(first_name, last_name=""):
+#     print("Hello {} {}".format(first_name, last_name))
+
+# myfunction("Irfan", "Juanda")
+# myfunction("Jelly")
+
+# def myfunction(first_name, last_name):
+#     print("Hello {} {}".format(first_name, last_name))
+
+# myfunction("Bambang", "Yudhoyono")
+# myfunction(last_name="Bambang",first_name="Yudhoyono")
+
+# def myfunction(child2,child1,child3):
+#     print("The youngest child is " + child3)
+
+# myfunction(child3="Emil",child2="Tobias",child1="Linus")
+
+# nama_buah = []
+
+# def tambah_nama_buah(nama):
+#     nama_buah.append(nama)
+#     print_nama_buah()
+
+# def print_nama_buah():
+#     for buah in nama_buah:
+#         print(buah)
+#     print("---")
+
+# tambah_nama_buah("apel")
+# tambah_nama_buah("jeruk")
+
+# def total(x , y):
+#     return x + y
+
+# print(total(10,15))
+
+def myfunction():
+    x =10
+    print ("Value inside function:", x)
+
+x = 20
+myfunction()
+print("Value outside function:", x)
+
