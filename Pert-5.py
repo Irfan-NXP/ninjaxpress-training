528db005c5e6dc220510acd56d6f16f119bae4c4
unknown <irfan.juanda@ninjavan.co>
2021-11-01 16:04:09 +0700

    pert-5

---
+++ Pert-5.py
@@ -0,0 +1,40 @@
+# MODULE
+# import mymodule
+
+# mymodule.greeting("Irfan")
+
+# import math
+
+# x = math.pi
+# print(x)
+
+# y = math.sqrt(64)
+# print(y)
+
+# FILE HANDLING
+
+# f = open("file.txt", "a")
+# f.write("Added text\n")
+# f.close()
+
+# f = open("file.txt", "a")
+# f.write("Hello!\n")
+# f.close()
+
+# f = open("file.txt", "r")
+# print(f.read())
+
+# f = open("file.txt", "r")
+# print(f.read(5))
+
+# f = open("file.txt", "r")
+# print(f.readline())
+
+# import os
+
+# os.remove("file.txt")
+
+import matematika
+
+print("PI:",matematika.pi)
+print(matematika.luas_persegi(12))
